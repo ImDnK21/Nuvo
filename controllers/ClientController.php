de13205ac911a81efb0439ba98c497420fcb3883
diegoeffar <diegoeffar@gmail.com>
2022-10-28 23:15:42 -0300

    a

--- controllers/ClientController.php
+++ controllers/ClientController.php
@@ -10,6 +10,8 @@ class ClientController{
         $vehicle = new Vehicle();
         $vehicle = $vehicle->getByOwner($_SESSION['logged']->RUT);
 
+        die(var_dump($vehicle));
+
         require_once('views/layout/sidebarClient.php');
         require_once('views/client/vehicle/index.php');
         // $vehicles = $vehicle->getByPatent($_SESSION['logged']->PATENT_VEHICLE);
