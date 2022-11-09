4cff04c00ecb1fd227164192923c1347286bc28c
Danko Sanchez <lhdyg2013@gmail.com>
2022-11-08 22:19:55 -0300

    asd

---
+++ views/home/index.php
@@ -0,0 +1,14 @@
+
+<div class="masthead">
+  <div>
+    <div class="mb-5">
+      <img src="<?= APP_URL . 'assets/img/logo.png' ?>"">
+    </div>
+    <h1 class="fw-bold">
+      Lorem ipsum
+    </h1>
+    <h3 class="mb-3">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ducimus!
+    </h3>
+  </div>
+</div>
