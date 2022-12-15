306b4941a713bdc55e5590c463252f0e61bea8c1
Danko Sanchez <lhdyg2013@gmail.com>
2022-12-15 16:01:49 -0300

    app

--- bd/nuvo-automotive2.sql
+++ bd/nuvo-automotive2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 13-12-2022 a las 22:45:20
+-- Tiempo de generación: 15-12-2022 a las 19:01:19
 -- Versión del servidor: 5.7.33
 -- Versión de PHP: 8.1.6
 
@@ -45,10 +45,16 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`ID`, `NAME_CATEGORY`) VALUES
-(3, 'Hoja'),
-(5, 'Herramientas'),
-(8, 'Agua descontaminante'),
-(14, 'Agua Desmineralizada');
+(15, 'Aceite de motor'),
+(16, 'Fusibles'),
+(17, 'Contrapesos'),
+(18, 'Aire Acondicionado'),
+(19, 'Guantes'),
+(20, 'Llaves de filtro'),
+(21, 'Llaves de impacto'),
+(22, 'Juego de alicates'),
+(23, 'Engrasadoras'),
+(24, 'Pulverizador');
 
 -- --------------------------------------------------------
 
@@ -69,6 +75,7 @@ INSERT INTO `commune` (`ID_COMMUNE`, `NOMBRE`) VALUES
 (1, 'La Florida'),
 (2, 'Cerrillos'),
 (3, 'Cerro Navia	'),
+(4, 'Conchali'),
 (5, 'El Bosque'),
 (6, 'Estacion Central'),
 (7, 'Huechuraba'),
@@ -96,6 +103,7 @@ INSERT INTO `commune` (`ID_COMMUNE`, `NOMBRE`) VALUES
 (31, 'Vitacura'),
 (32, 'Puente Alto'),
 (33, 'Pirque'),
+(34, 'San jose de maipo'),
 (35, 'Colina'),
 (36, 'Lampa'),
 (37, 'San Bernardo\"'),
@@ -189,8 +197,7 @@ CREATE TABLE `supply` (
 --
 
 INSERT INTO `supply` (`ID_SUPPLY`, `ID_CATEGORY`, `NAME`, `DESCRIPTION`, `STOCK`, `IMG`, `CREATED_AT`, `UPDATED_AT`) VALUES
-(1, 5, 'Juego De Llaves', 'Juego de llaves del N~ 1 al 12', 2, 'herramientas_juego-de-llaves.png', '2022-11-30 01:46:00', '2022-11-30 01:46:00'),
-(2, 3, 'Lija', 'Lija para lijar', 12, '', '2022-12-02 05:41:30', '2022-12-02 05:41:30');
+(1, 19, 'Guantes Cabritilla', 'Guante de cuero elasticado', 4, 'guantes_guantes-cabritilla.png', '2022-12-14 02:50:59', '2022-12-14 02:50:59');
 
 -- --------------------------------------------------------
 
@@ -237,13 +244,14 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`ROLE`, `RUT`, `FIRSTNAME`, `LASTNAME`, `PHONE`, `ADDRESS`, `ID_COMMUNE`, `EMAIL`, `PASSWORD`, `CREATED_AT`, `UPDATED_AT`) VALUES
+('client', '13918256-5', 'Angel Roberto', 'Miranda Casas', '+56928327292', 'Pedreros #9834', 31, 'angel@gmail.com', '$2y$10$m89se99iFn7/itc1VUFU8uMyPVufDzP6QftSfpv4xVzzOM3XYcNqC', '2022-12-14 22:37:18', '2022-12-15 02:07:11'),
+('mechanic', '17672942-5', 'Rodolfo Martin', 'Molina Castro', '+56982374821', 'Av San jose de la estrella #982', 1, 'rodolfo@gmail.com', '$2y$10$CZkm5gdzJ.o96rNxOs6cauQFoScL25NzqE34Yf05V5h2UxvDbdke6', '2022-12-15 01:58:56', '2022-12-15 01:59:29'),
 ('mechanic', '18234023-4', 'Luis Antonio', 'Miranda Tunnineti', '+56982734123', 'Av Uno #5112', 9, 'luis@gmail.com', '$2y$10$UZqZRMmRcBY6NTbBTQdKDuoUPLTl7SBSj37imKhQ2pTR13gt/1EfS', '2022-12-08 04:54:34', '2022-12-08 04:54:34'),
-('mechanic', '18239423-2', 'Joel Antonio', 'Sanhueza Diaz', '82183213', 'jdajsj', 1, 'asd@asd', '$2y$10$LsHd.KCpiMhpoqIK0my2cOSmP/F4TIPiEbNZHcZVAqn7BgBtilSuu', '2022-12-09 05:47:39', '2022-12-09 05:47:39'),
+('mechanic', '18239423-2', 'Joel Antonio', 'Sanhueza Diaz', '82183213', 'Santa isabel #5982', 27, 'joel@gmail.com', '$2y$10$LsHd.KCpiMhpoqIK0my2cOSmP/F4TIPiEbNZHcZVAqn7BgBtilSuu', '2022-12-09 05:47:39', '2022-12-15 01:57:26'),
 ('client', '19823923-2', 'Andre Antonia', 'Riquelme Soto', '+56928342723', 'San jose De La Estrella #123', 12, 'andrea@gmail.com', '$2y$10$RpuejO9fMhqFiv4BAde4.u7Kln/Oo8EhcpsydTAEMjF5djAnqi1WO', '2022-12-09 01:58:11', '2022-12-09 04:08:00'),
-('client', '20049428-8', 'Gary Sebastian', 'Sandia Perez', '+56982371231', 'Gounod #10232', 31, 'gary@gmail.com', '$2y$10$Z5cJhIuVKvT76KOWdsfsGeu2fCdnDacBgNdUGJuH0UDt01Bh3tcm.', '2022-12-07 02:19:46', '2022-12-07 02:19:46'),
 ('admin', '20049429-6', 'Danko Sebastian', 'Sanchez Arancibia', '+56982731322', '', 1, 'admin@admin.cl', '$2y$10$whvSq7ZWEc2Q3Jajb38dHuBq5ZzudHvO8cLr269o0ZYszeS5KwOPm', '2022-12-07 00:37:02', '2022-12-09 04:06:56'),
-('client', '28123923-2', 'Ernesto Osvaldo', 'Sandoval Diaz', '+56982384232', 'jadsj #1238', 5, 'ernesto@gmail.com', '$2y$10$we1x2G5Deg0ryOHRx7PhDejzcNkWF2BbZ6oxdEU5ntt6VhvuJcbou', '2022-12-09 03:09:38', '2022-12-09 03:09:38'),
-('client', '29928321-2', 'Diego Andres', 'Molina Castro', '+56928321422', 'Porto #5122', 5, 'molina@gmail.com', '$2y$10$6uirKdUlF42I3YRbpiqd1eKdxt2pEDk3hwJ4yoJnYF0bnmwnKQbai', '2022-12-07 01:28:13', '2022-12-07 05:03:19');
+('client', '20092842-4', 'Bastian Osvaldo', 'Perez Soto', '+56928138231', 'Av Palicano #512', 34, 'bastian@gmail.com', '$2y$10$7YaKid/3mlJ1roAmYXPw5ONc2MaMGqaIKET9FTRn13V4g7j197K82', '2022-12-15 01:55:26', '2022-12-15 01:55:26'),
+('client', '28123923-2', 'Ernesto Osvaldo', 'Sandoval Diaz', '+56982384232', 'jadsj #1238', 5, 'ernesto@gmail.com', '$2y$10$we1x2G5Deg0ryOHRx7PhDejzcNkWF2BbZ6oxdEU5ntt6VhvuJcbou', '2022-12-09 03:09:38', '2022-12-09 03:09:38');
 
 -- --------------------------------------------------------
 
@@ -272,7 +280,11 @@ CREATE TABLE `vehicle` (
 --
 
 INSERT INTO `vehicle` (`PATENT`, `OWNER`, `BRAND`, `MODEL`, `YEAR`, `ID_FUEL_TYPE`, `ID_TRANSMISSION`, `COLOR`, `CHASSIS_NUMBER`, `MILEAGE`, `ID_TYPE_VEHICLE`, `CREATED_AT`, `UPDATED_AT`) VALUES
-('MALS-23', '28123923-2', 'Nissan', 'V16', '2021', 3, 2, '#000000', '#A842132', '20942', 6, '2022-12-10 05:31:48', '2022-12-10 05:31:48');
+('JSPR-52', '19823923-2', 'Nissan', '350z', '2019', 2, 2, '#000000', '#A985823', '5232', 5, '2022-12-13 22:52:56', '2022-12-13 22:52:56'),
+('KSPR-52', '13918256-5', 'Lamborgini', 'Spider', '2021', 1, 1, '#000000', '#232412231', '23132', 5, '2022-12-14 21:54:03', '2022-12-14 22:38:39'),
+('LDPA-21', '28123923-2', 'Honda', 'CRV', '2022', 1, 1, '#000000', '#A512321312', '45213', 4, '2022-12-13 23:21:38', '2022-12-14 22:20:47'),
+('MSUR-98', '20092842-4', 'Supra', 'Ibiza', '2022', 1, 2, '#000000', '#A82374232', '24232', 5, '2022-12-15 02:21:58', '2022-12-15 02:21:58'),
+('PSRL-45', '13918256-5', 'Nissan', 'Skyline', '2021', 1, 1, '#000000', '#A52131232', '423213', 3, '2022-12-14 22:38:28', '2022-12-14 22:38:28');
 
 -- --------------------------------------------------------
 
@@ -321,7 +333,8 @@ CREATE TABLE `workorder` (
 --
 
 INSERT INTO `workorder` (`ID`, `PATENT_VEHICLE`, `RUT_CLIENT`, `RUT_MECHANIC`, `OBSERVATIONS`, `ID_STATUS`, `CREATED_AT`, `UPDATED_AT`) VALUES
-(3, 'MALS-23', '28123923-2', '18234023-4', '- Vehiculo sin tapabarros delantero izquierdo\r\n- Radio en mal estado', 1, '2022-12-10 05:35:17', '2022-12-10 05:35:17');
+(8, 'JSPR-52', '19823923-2', '18234023-4', '- Vehiculo cuenta con vidrio delantero izquierdo trizado\r\n\r\n- Cliente solicitado cambio de aceite de motor', 1, '2022-12-14 05:14:08', '2022-12-14 05:14:08'),
+(9, 'LDPA-21', '28123923-2', '18239423-2', '- Cliente Solicita Cambio de aceite de motor\r\n- Vehiculo ingresado sin tapabarros', 1, '2022-12-14 22:44:24', '2022-12-14 22:44:24');
 
 -- --------------------------------------------------------
 
@@ -357,7 +370,13 @@ INSERT INTO `wo_service` (`ID`, `ID_SERVICE`, `ID_WO`) VALUES
 (15, '2', '5'),
 (16, '6', '5'),
 (17, '6', '6'),
-(18, '7', '6');
+(18, '7', '6'),
+(19, '6', '7'),
+(20, '7', '7'),
+(21, '2', '8'),
+(22, '6', '8'),
+(23, '7', '9'),
+(24, '8', '9');
 
 --
 -- Índices para tablas volcadas
@@ -455,7 +474,7 @@ ALTER TABLE `wo_service`
 -- AUTO_INCREMENT de la tabla `category`
 --
 ALTER TABLE `category`
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de la tabla `service`
@@ -467,19 +486,19 @@ ALTER TABLE `service`
 -- AUTO_INCREMENT de la tabla `supply`
 --
 ALTER TABLE `supply`
-  MODIFY `ID_SUPPLY` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID_SUPPLY` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `workorder`
 --
 ALTER TABLE `workorder`
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `wo_service`
 --
 ALTER TABLE `wo_service`
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- Restricciones para tablas volcadas
